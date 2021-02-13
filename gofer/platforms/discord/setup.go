d2a933700a1437916df1863ea6cbc3af21018f5d
Srevin Saju <srevinsaju@sugarlabs.org>
2021-02-14 00:42:15 +0300

    feat: remove unused function DiscordEventHandler

--- gofer/platforms/discord/setup.go
+++ gofer/platforms/discord/setup.go
@@ -1,32 +1,10 @@
 package discord
 
 import (
-	"errors"
-	"github.com/bwmarrin/discordgo"
+	"github.com/withmandala/go-log"
+	"os"
 )
 
-
-func DiscordSetup(ctx *Context) error {
-
-	discordBotToken := ctx.config.DiscordApiToken
-	if discordBotToken == "" {
-		err := errors.New("[DiscordBot] config[\"discord_api_token\"] is not provided")
-		return err
-	}
-
-	discordBot, err := discordgo.New("Bot " + discordBotToken)
-	if err != nil {
-		return err
-	}
-
-	ctx.discordBot = discordBot
-
-	ctx.discordBot.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
-		DiscordOnMessageHandler(ctx, s, m)
-	})
-	ctx.discordBot.Identify.Intents = discordgo.IntentsGuildMessages
-
-	return nil
-}
+var logger = log.New(os.Stdout)
 
 
--- gofer/platforms/discord/utils.go
+++ gofer/platforms/discord/utils.go
@@ -3,11 +3,9 @@ package discord
 import (
 	"errors"
 	"github.com/srevinsaju/gofer/types"
-	"github.com/withmandala/go-log"
-	"os"
 )
 
-var logger = log.New(os.Stdout)
+
 
 
 func GetChannels(chanId string, channels map[string]types.ChannelMapping) (types.ChannelMapping, error) {
